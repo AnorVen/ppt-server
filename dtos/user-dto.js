c492e293ee2419a843ba7c3dbe6a1092929d0a46
Ефремов Георгий Станиславович <g.efremov@t1-group.ru>
2023-10-21 13:06:39 +0100

    fix

--- dtos/user-dto.js
+++ dtos/user-dto.js
@@ -1,9 +1,45 @@
 export class UserDto {
-    email;
-    id;
+	email;
+	id;
+	phone;
+	name;
+	second_name;
+	surname;
+	city;
+	description;
+	photo;
+	certificate;
+	master;
+	superUser;
+	type;
 
-    constructor(model) {
-        this.email = model.email;
-        this.id = model._id;
-    }
+	constructor({
+		            email,
+                    _id,
+		            phone,
+		            name,
+		            second_name,
+		            surname,
+		            city,
+		            description,
+		            photo,
+		            certificate,
+		            master,
+		            superUser,
+		            type,
+	            }) {
+		this.email = email;
+		this.id = _id;
+		this.phone = phone;
+		this.name = name;
+		this.second_name = second_name;
+		this.surname = surname;
+		this.city = city;
+		this.description = description;
+		this.photo = photo;
+		this.certificate = certificate;
+		this.master = master;
+		this.superUser = superUser;
+		this.type = type;
+	}
 }
